0b416d790609994cbaa66c4f9920f22147c2295b
Terer <samahpowers@gmail.com>
2023-08-05 07:05:26 +0300

    1st commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+const addUserform  = document.querySelector(".add")
+const forms = document.querySelector(".forms")
+const signUp = document.getElementById("sup")
+const main =document.querySelector(".main")
+
+const signInModel = document.querySelector(".form-si")
+const signInForm = document.querySelector(".si")
+const signInbutton = document.querySelector("#sibtn")
+
+document.addEventListener("click", (e)=>{
+if(signUp.contains(e.target)){
+forms.style.display = "block"
+}else if(forms.contains(e.target)){
+  forms.style.display = "block"
+  
+} else {
+  forms.style.display = "none"
+}
+if(signInbutton.contains(e.target)){
+  signInModel.style.display = "block"
+  
+}else if(signInForm.contains(e.target)){
+signInModel.style.display = "block"
+} else{
+  signInModel.style.display = "none"
+}
+})
+
+
+document.getElementById('userForm').addEventListener('submit', async function(e) {
+            e.preventDefault();
+
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const phone = document.getElementById('phone').value;
+            
+            const data = { name, email, phone };
+            console.log(data)
+            try {
+                const response = await fetch("http://localhost:5001/api/users/", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                const user = await response.json();
+                console.log('User created:', user);
+            } catch (error) {
+                console.error('Error creating user:', error.message);
+            }
+        });
+        
